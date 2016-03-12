4ae8f3037587ee17f0e76b63fb741a7018bb1e60
Krit <ac127_44264@hotmail.com>
2016-03-12 13:16:28 +0700

    test

---
+++ main.js
@@ -0,0 +1,25 @@
+/*var calculator = require('./calculator')
+
+console.log(calculator.add(10,20))
+console.log(calculator.minus(10,20))
+console.log(calculator.multiply(10,20))
+console.log(calculator.divide(10,20))
+*/
+
+
+/*
+require('./lib/hello.js')
+require('./lib/hello.js')
+require('./lib/hello.js')
+*/
+
+/*require('./lib/')*/
+
+/*
+var cal = require('ywcworkshop-poh')
+console.log(cal);
+*/
+
+var fs = require('fs')
+var os = require('os')
+console.log(fs)
